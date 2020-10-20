a5a7c661c4013e7118e00f6483183e8e2ff8d540
tigyiandras <71698671+tigyiandras@users.noreply.github.com>
2020-10-20 16:59:59 +0200

    Create f0003b.py

---
+++ f0003b.py
@@ -0,0 +1,4 @@
+vezetéknév = input(" Mi a vezetékneved? ")
+Keresztnév = input ( " Mi a keresztneved? ")
+év = input ('melyik évben születtél? ')
+print ('A te neved ', vezetéknév,' ', keresztnév, '.', sep='')
