b102f8aaf299b03d825e797eb1deac4c01905873
tigyiandras <71698671+tigyiandras@users.noreply.github.com>
2020-10-20 17:01:37 +0200

    Delete f0003b.py

--- f0003b.py
+++
@@ -1,4 +0,0 @@
-vezetéknév = input(" Mi a vezetékneved? ")
-Keresztnév = input ( " Mi a keresztneved? ")
-év = input ('melyik évben születtél? ')
-print ('A te neved ', vezetéknév,' ', keresztnév, '.', sep='')
