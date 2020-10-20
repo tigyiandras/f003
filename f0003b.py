b79a6c3384ce3f416fdfe4befbd0ac3549bda74d
tigyiandras <71698671+tigyiandras@users.noreply.github.com>
2020-10-20 17:13:23 +0200

    Create f0003b.py

---
+++ f0003b.py
@@ -0,0 +1,6 @@
+vezetéknév = input ('Mi a vezetékneved? ')
+keresztnév = input ('Mi a keresztneved? ')
+születésiév = input ('Mikor szulettél?')
+csillagjegy = input ('Mi a csillag jegyed?')
+print('a te neved', vezetéknév, keresztnév, )
+print('születési éved',születésiév, 'csillag jegyed' ,csillagjegy ,)
